e65383cd597f207a9e60cd14c222841b20ef63fe
Angie <angieibarrola@gmail.com>
2024-11-22 13:10:14 +0100

    basic JS

---
+++ JS fundam/changes.js
@@ -0,0 +1,17 @@
+let theImg = document.querySelector("img");
+
+function toBunny(){
+	document.getElementById('bear').src='bunny.jpg';
+	document.getElementById('bear').id='bunny';
+}
+function changeH1(){
+	document.querySelectorAll('h1')[0].innerHTML="Cute bunny";
+}
+
+function toBear(){
+	document.getElementById('bunny').src='bear.jpg';
+	document.getElementById('bunny').id='bear';
+}
+function changeH2(){
+	document.querySelectorAll('h1')[0].innerHTML="Cute bear";
+}
